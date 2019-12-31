c420e7fd1000b2259e5c5c5b595e3aa39e3222a8
CielSeven <38318346+CielSeven@users.noreply.github.com>
2019-12-31 13:54:12 +0800

    Add files via upload

---
+++ Final_dataset/test.py
@@ -0,0 +1,183 @@
+import os
+
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+
+import pandas as pd
+import time
+import datetime
+import random
+import numpy as np
+from sklearn.model_selection import train_test_split
+import torch
+from keras.preprocessing.sequence import pad_sequences
+from transformers import BertTokenizer
+from transformers import BertForSequenceClassification, AdamW, BertConfig
+
+
+
+
+# Prediction on test set
+
+df = pd.read_pickle('../Final_dataset/test_data.pkl').reset_index(drop= True)
+print('Predicting labels for {:,} test sentences...'.format(len(df)))
+print(df)
+print('Loading BERT tokenizer...')
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
+
+model = BertForSequenceClassification.from_pretrained(
+    "../Final_dataset/model_save/", # Use the 12-layer BERT model, with an uncased vocab.
+    num_labels = 64, # The number of output labels--2 for binary classification.
+                    # You can increase this for multi-class tasks.
+    output_attentions = False, # Whether the model returns attentions weights.
+    output_hidden_states = False, # Whether the model returns all hidden-states.
+)
+
+# Tell pytorch to run this model on the GPU.
+
+
+model.cuda()
+
+# Put model in evaluation mode
+model.eval()
+
+# Tracking variables
+predictions, true_labels = [], []
+
+
+# Report the number of sentences.
+print('Number of test sentences: {:,}\n'.format(df.shape[0]))
+
+# Create sentence and label lists
+sentences = df.texts.values
+labels = df.label.values.tolist()
+labels = np.array([ x - 1 for x in labels])
+labels = labels.astype(int)
+
+# Tokenize all of the sentences and map the tokens to thier word IDs.
+input_ids = []
+
+# For every sentence...
+for sent in sentences:
+    # `encode` will:
+    #   (1) Tokenize the sentence.
+    #   (2) Prepend the `[CLS]` token to the start.
+    #   (3) Append the `[SEP]` token to the end.
+    #   (4) Map tokens to their IDs.
+    encoded_sent = tokenizer.encode(
+        sent,  # Sentence to encode.
+        max_length = 128,
+        add_special_tokens=True,  # Add '[CLS]' and '[SEP]'
+    )
+
+    input_ids.append(encoded_sent)
+
+# Pad our input tokens
+input_ids = pad_sequences(input_ids, maxlen= 138,
+                          dtype="long", truncating="post", padding="post")
+
+# Create attention masks
+attention_masks = []
+
+# Create a mask of 1s for each token followed by 0s for padding
+for seq in input_ids:
+    seq_mask = [float(i > 0) for i in seq]
+    attention_masks.append(seq_mask)
+
+# Convert to tensors.
+prediction_inputs = torch.tensor(input_ids)
+prediction_masks = torch.tensor(attention_masks)
+prediction_labels = torch.tensor(labels)
+
+# Set the batch size.
+batch_size = 32
+
+# Create the DataLoader.
+prediction_data = TensorDataset(prediction_inputs, prediction_masks, prediction_labels)
+prediction_sampler = SequentialSampler(prediction_data)
+prediction_dataloader = DataLoader(prediction_data, sampler=prediction_sampler, batch_size=batch_size)
+
+device = torch.device('cuda')
+
+def flat_accuracy(preds, labels):
+    pred_flat = np.argmax(preds, axis=1).flatten()
+    labels_flat = labels.flatten()
+    return np.sum(pred_flat == labels_flat) / len(labels_flat)
+
+
+# Predict
+eval_accuracy = 0
+nb_eval_steps, nb_eval_examples = 0, 0
+i = 0
+for batch in prediction_dataloader:
+    # Add batch to GPU
+    batch = tuple(t.to(device) for t in batch)
+
+    # Unpack the inputs from our dataloader
+    b_input_ids, b_input_mask, b_labels = batch
+
+    # Telling the model not to compute or store gradients, saving memory and
+    # speeding up prediction
+    with torch.no_grad():
+        # Forward pass, calculate logit predictions
+        outputs = model(b_input_ids, token_type_ids=None,
+                        attention_mask=b_input_mask)
+
+    logits = outputs[0]
+
+    # Move logits and labels to CPU
+    logits = logits.detach().cpu().numpy()
+    label_ids = b_labels.to('cpu').numpy()
+
+    tmp_eval_accuracy = flat_accuracy(logits, label_ids)
+
+    # Accumulate the total accuracy.
+    eval_accuracy += tmp_eval_accuracy
+
+    # Track the number of batches
+    nb_eval_steps += 1
+
+    # Store predictions and true labels
+    predictions.append(logits)
+    true_labels.append(label_ids)
+    # i +=1
+    # if i > 10:
+    #     break
+
+
+
+print('    DONE.')
+#
+# print(predictions)
+#
+# print('\n')
+# print(true_labels)
+print("  Accuracy: {0:.2f}".format(eval_accuracy / nb_eval_steps))
+
+from sklearn.metrics import matthews_corrcoef
+
+matthews_set = []
+
+# Evaluate each test batch using Matthew's correlation coefficient
+print('Calculating Matthews Corr. Coef. for each batch...')
+
+# For each input batch...
+for i in range(len(true_labels)):
+    # The predictions for this batch are a 2-column ndarray (one column for "0"
+    # and one column for "1"). Pick the label with the highest value and turn this
+    # in to a list of 0s and 1s.
+    pred_labels_i = np.argmax(predictions[i], axis=1).flatten()
+
+    # Calculate and store the coef for this batch.
+    matthews = matthews_corrcoef(true_labels[i], pred_labels_i)
+    matthews_set.append(matthews)
+# Combine the predictions for each batch into a single list of 0s and 1s.
+flat_predictions = [item for sublist in predictions for item in sublist]
+flat_predictions = np.argmax(flat_predictions, axis=1).flatten()
+
+# Combine the correct labels for each batch into a single list.
+flat_true_labels = [item for sublist in true_labels for item in sublist]
+
+# Calculate the MCC
+mcc = matthews_corrcoef(flat_true_labels, flat_predictions)
+
+print('MCC: %.3f' % mcc)
